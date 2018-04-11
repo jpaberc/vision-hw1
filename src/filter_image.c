2defe3deed072781aaa1fc6b9c0e4400b63b96e2
John Abercrombie <jpaberc@cs.washington.edu>
2018-04-10 23:12:29 -0700

    done? waiting for tests

--- src/filter_image.c
+++ src/filter_image.c
@@ -54,7 +54,7 @@ image convolve_image(image im, image filter, int preserve)
                             }
                         }
                     }
-                    q = (q > 0.0) ? ((q < 1.0) ? q : 1.0) : 0.0;
+                    // q = (q > 0.0) ? ((q < 1.0) ? q : 1.0) : 0.0;
                     set_pixel(cim,x,y,0,q);
                 }
             }
@@ -71,7 +71,7 @@ image convolve_image(image im, image filter, int preserve)
                                 q += filter_val*pixel_val;
                             }
                         }
-                        q = (q > 0.0) ? ((q < 1.0) ? q : 1.0) : 0.0;
+                        // q = (q > 0.0) ? ((q < 1.0) ? q : 1.0) : 0.0;
                         set_pixel(cim,x,y,c,q);
                     }
                 }
@@ -92,7 +92,7 @@ image convolve_image(image im, image filter, int preserve)
                         }
                     }
                     q = q / im.c;
-                    q = (q > 0.0) ? ((q < 1.0) ? q : 1.0) : 0.0;
+                    // q = (q > 0.0) ? ((q < 1.0) ? q : 1.0) : 0.0;
                     set_pixel(cim,x,y,0,q);
                 }
             }
@@ -108,7 +108,7 @@ image convolve_image(image im, image filter, int preserve)
                                 q += filter_val*pixel_val;
                             }
                         }
-                        q = (q > 0.0) ? ((q < 1.0) ? q : 1.0) : 0.0;
+                        // q = (q > 0.0) ? ((q < 1.0) ? q : 1.0) : 0.0;
                         set_pixel(cim,x,y,c,q);
                     }
                 }
@@ -209,47 +209,152 @@ image make_emboss_filter()
 
 image make_gaussian_filter(float sigma)
 {
-    // TODO
-    return make_image(1,1,1);
+    float G,gx,gy;
+    image f = make_box_filter((6*sigma) + 1);
+    for (int y = 0; y < f.h; y++) {
+        for (int x = 0; x < f.w; x++) {
+            gx = x - 3*sigma;
+            gy = y - 3*sigma;
+            G = (1.0/(TWOPI*powf(sigma,2))) * powf(M_E, -1.0*(powf(gx,2)+powf(gy,2))/(2*powf(sigma,2)));
+            set_pixel(f,x,y,0,G);
+        }
+    }
+    return f;
 }
 
 image add_image(image a, image b)
 {
-    // TODO
-    return make_image(1,1,1);
+    assert(a.w == b.w && a.h == b.h && a.c == b.c);
+    float new_val;
+    image a_plus_b = make_image(a.w,a.h,a.c);
+    for (int c = 0; c < a.c; c++) {
+        for (int y = 0; y < a.h; y++) {
+            for (int x = 0; x < a.w; x++) {
+                new_val = get_pixel(a,x,y,c) + get_pixel(b,x,y,c);
+                // new_val = (new_val > 1.0) ? 1.0 : new_val;
+                set_pixel(a_plus_b,x,y,c,new_val);
+            }
+        }
+    }
+    return a_plus_b;
 }
 
 image sub_image(image a, image b)
 {
-    // TODO
-    return make_image(1,1,1);
+    assert(a.w == b.w && a.h == b.h && a.c == b.c);
+    float new_val;
+    image a_minus_b = make_image(a.w,a.h,a.c);
+    for (int c = 0; c < a.c; c++) {
+        for (int y = 0; y < a.h; y++) {
+            for (int x = 0; x < a.w; x++) {
+                new_val = get_pixel(a,x,y,c) - get_pixel(b,x,y,c);
+                // new_val = (new_val < 0.0) ? 0.0 : new_val;
+                set_pixel(a_minus_b,x,y,c,new_val);
+            }
+        }
+    }
+    return a_minus_b;
 }
 
 image make_gx_filter()
 {
-    // TODO
-    return make_image(1,1,1);
+    image f = make_box_filter(3);
+    set_pixel(f,0,0,0,-1);
+    set_pixel(f,1,0,0,0);
+    set_pixel(f,2,0,0,1);
+    set_pixel(f,0,1,0,-2);
+    set_pixel(f,1,1,0,0);
+    set_pixel(f,2,1,0,2);
+    set_pixel(f,0,2,0,-1);
+    set_pixel(f,1,2,0,0);
+    set_pixel(f,2,2,0,1);
+    return f;
 }
 
 image make_gy_filter()
 {
-    // TODO
-    return make_image(1,1,1);
+    image f = make_box_filter(3);
+    set_pixel(f,0,0,0,-1);
+    set_pixel(f,1,0,0,-2);
+    set_pixel(f,2,0,0,-1);
+    set_pixel(f,0,1,0,0);
+    set_pixel(f,1,1,0,0);
+    set_pixel(f,2,1,0,0);
+    set_pixel(f,0,2,0,1);
+    set_pixel(f,1,2,0,2);
+    set_pixel(f,2,2,0,1);
+    return f;
 }
 
 void feature_normalize(image im)
 {
-    // TODO
+    float v;
+    float min = INFINITY;
+    float max = -1.0 * INFINITY;
+    // find min and max
+    for (int c = 0; c < im.c; c++) {
+        for (int y = 0; y < im.h; y++) {
+            for (int x = 0; x < im.w; x++) {
+                v = get_pixel(im,x,y,c);
+                min = (v < min) ? v : min;
+                max = (v > max) ? v : max;
+            }
+        }
+    }
+    float range = max - min;
+
+    // subtract min, divide by the range
+    for (int c = 0; c < im.c; c++) {
+        for (int y = 0; y < im.h; y++) {
+            for (int x = 0; x < im.w; x++) {
+                set_pixel(im,x,y,c, (get_pixel(im,x,y,c)-min) / range);
+            }
+        }
+    }
 }
 
 image *sobel_image(image im)
 {
-    // TODO
-    return calloc(2, sizeof(image));
+    image gx = make_gx_filter();
+    image gy = make_gy_filter();
+
+    image * sobel = calloc(2, sizeof(image));
+
+    image xsobel = convolve_image(im,gx,0);
+    image ysobel = convolve_image(im,gy,0);
+
+    sobel[0] = make_image(im.w,im.h,1);
+    sobel[1] = make_image(im.w,im.h,1);
+    for (int y = 0; y < im.h; y++) {
+        for (int x = 0; x < im.w; x++) {
+            float mag = sqrtf( powf(get_pixel(xsobel,x,y,0),2) + powf(get_pixel(ysobel,x,y,0),2) );
+            float dir = atanf(get_pixel(ysobel,x,y,0) / get_pixel(xsobel,x,y,0));
+            set_pixel(sobel[0],x,y,0,mag);
+            set_pixel(sobel[1],x,y,0,dir);
+        }
+    }
+
+    return sobel;
 }
 
 image colorize_sobel(image im)
 {
-    // TODO
-    return make_image(1,1,1);
+    image * sobel = sobel_image(im);
+    feature_normalize(sobel[0]);
+    feature_normalize(sobel[1]);
+
+    image csobel = make_image(im.w,im.h,3);
+    for (int c = 0; c < csobel.c; c++) {
+        for (int y = 0; y < csobel.h; y++) {
+            for (int x = 0; x < csobel.w; x++) {
+                if (c == 0) {
+                    set_pixel(csobel,x,y,c,get_pixel(sobel[1],x,y,0));
+                } else {
+                    set_pixel(csobel,x,y,c,get_pixel(sobel[0],x,y,0));
+                }
+            }
+        }
+    }
+    hsv_to_rgb(csobel);
+    return convolve_image(csobel,make_gaussian_filter(1),1);
 }
